3fd3ca9eef9662874b8c04fcb45363b89c78ebb6
Babirye Grace <41687107+babiryegrace20@users.noreply.github.com>
2020-02-05 18:05:30 +0300

    Add files via upload
    Grace's Paye exercise

---
+++ BabiryeGrace.js
@@ -0,0 +1,7 @@
+function paye(){ //function definition
+  let income = 1000000 //initialising the income pay
+  let tax = 30/100     //initialising the tax incurred
+  let paye = income * tax  //formula for calculating paye
+  console.log(paye)
+}
+paye() //calling the paye function
