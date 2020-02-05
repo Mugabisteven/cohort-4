4087e102c491f3e9d883344ea115510aa8427dc5
camillus78 <60211082+camillus78@users.noreply.github.com>
2020-02-05 05:42:19 -0800

    camillus78

---
+++ mynum.js
@@ -0,0 +1,17 @@
+function paye() 
+{
+let intSal = 10000000
+let  numOne = 30
+let numTwo =100
+let ansOne = numOne/numTwo
+ 
+let payee =ansOne*intSal 
+let payNet = intSal-payee
+console.log(payee,payNet)
+
+}
+paye()
+
+
+
+
