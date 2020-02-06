53bdae1e54ac0c48e154d831890facbc4d3b39a4
ashe-senkatuuka <ashemukisa@gmail.com>
2020-02-06 10:28:02 +0300

    modified the netPay function to receive arguments

--- ashe_uganda_netPay.js
+++ ashe_uganda_netPay.js
@@ -1,20 +1,15 @@
-//declaring and defining a function which calculates the net salary
-function netPay(){
-	//variable to store the total amount of an employees salary
-	var totalSalary = 4000000
-
-	//variable to store the uganda revenue tax rate
-	var uraRate = 30/100
-
+//declaring and defining a parameterized function which calculates the net salary
+function netPay(totalSalary, uraRate){
 	//calculating the tax amount and assigning it to a varibale
-	var uraDues = totalSalary * uraRate
+	var uraDues = (uraRate/100) *totalSalary
 
 	//subtracting the tax amount from the total amount paid to the employee
 	var ugNetPay = totalSalary - uraDues
 
 	//print the net payment to the screen
-	console.log(ugNetPay)
+	console.log("Your net salary is " + ugNetPay )
+	console.log("after reducing tax amount of " + uraDues)
 }
 
-//call the netPay function
-netPay()
+//call the netPay function parsing two arguments
+netPay(1000000,30)
