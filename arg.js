d9a545bc9692a3f49381bd2d46b780ef6622e97e
BatoAlvin <60175917+BatoAlvin@users.noreply.github.com>
2020-02-06 18:28:47 +0300

    Bato Alvin
    Used arg

---
+++ arg.js
@@ -0,0 +1,15 @@
+function wages(rate,income)
+{
+    //calculating payee
+var payee = rate*income
+//calculating netPay
+var netPay = income-payee
+//calculated payee
+console.log("Here is payee:")
+//printed payee
+console.log(payee)
+console.log("Here is netPay:")
+//printed netPay
+console.log(netPay)
+}
+wages(30/100,70000)
