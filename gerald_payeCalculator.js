5b8107ba57e4af8cdef18ce17d5886541413b572
Gerald Ssali <57569353+gs-maker@users.noreply.github.com>
2020-02-07 08:47:59 +0300

    Update gerald_payeCalculator.js

--- gerald_payeCalculator.js
+++ gerald_payeCalculator.js
@@ -12,3 +12,17 @@ function paye() {
 }
 paye()
 
+
+//February 6th 
+function payeeCalc(grossPay, payePerc) {
+   let tax = payePerc / 100
+   let taxDeduct = grossPay * tax
+   return taxDeduct
+}
+
+function netPay(grossPay, taxDeduct) {
+   let salo = grossPay - taxDeduct
+   console.log(salo)
+}
+let payee = payeeCalc(1000000, 20)
+netPay(1000000, payee)
