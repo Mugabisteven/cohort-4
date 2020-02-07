43b1b6335103623c3f02cb246de1d36aab5e133d
Otimkev <kevinotim4@gmail.com>
2020-02-07 09:44:08 -0500

    netpay calculation

--- otimKevin.js
+++ otimKevin.js
@@ -1,11 +1,9 @@
 //function name is netPay
 //taxRate is equal to 30%
 //netpay is equal to grossPay-net_pay
-//gross pay is equal to 1000000
-
 function netPay(gross){
     let taxRate = 0.3;
     let net_pay = gross -(taxRate*gross)
     console.log(net_pay)
 }
-netPay(1000000);
+netPay(1000000)// calling netPay function and setting grossPay to 1000000
