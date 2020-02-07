ad94464d88b2ef233c1fea04424abdb5506a3915
akapesh <60213676+akapesh@users.noreply.github.com>
2020-02-07 08:56:53 +0300

    parameterised function

--- Pesh_A.js
+++ Pesh_A.js
@@ -2,7 +2,7 @@ function paye()
 {
     //initialising gross income
     let grossIncome = 2500000
-    //initialising taxrate
+    //initialising taxrate 
     let taxRate = 30/100
 
     //assigning the product of gross income and taxrate
@@ -11,8 +11,20 @@ function paye()
     let netPay = grossIncome - tax
 
     //prints out the product
+    console.log("Your tax is : ")
     console.log(tax)
     //prints out the difference
+    console.log("Your netPay is : ")
     console.log(netPay)
 }
 paye()
+
+//parameterised function
+function payeParam(grossIncome, taxRate)
+{
+  let tax = grossIncome * taxRate/100
+  console.log("Your tax is : " + tax)
+  let netPay = grossIncome - tax
+  console.log("Your netPay is : " + netPay)
+}
+payeParam(2500000, 30)
