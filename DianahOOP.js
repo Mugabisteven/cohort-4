184358a7a35336bd42322b6c44b66000694f628f
Dianah-code <60173686+Dianah-code@users.noreply.github.com>
2020-02-10 16:43:05 +0300

    Add files via upload

---
+++ DianahOOP.js
@@ -0,0 +1,41 @@
+//Defining Object tree with five properties
+let tree={type:"coniferous",local_name:"mutuba",engname:"fnatalensis",rank:"specie",location:"Uganda"}
+//Printing the 5 five properties of tree object
+console.log(tree.type)
+console.log(tree.local_name)
+console.log(tree.engname)
+console.log(tree.rank)
+console.log(tree.location)
+//Defining Object soil with five properties
+let soil={location:"central",depth:"subsoil",color:"red", texture:"clay", pH:"acidic"}
+//Printing the 5 five properties of soil object
+console.log(soil.location)
+console.log(soil.depth)
+console.log(soil.color)
+console.log(soil.texture)
+console.log(soil.pH)
+//Defining Object shop with five properties
+let shop={type:"boutique",stock:"clothes",location:"Ntinda",status:"active", oprtn:"fulltime"}
+//Printing the 5 five properties of shop object
+console.log(shop.type)
+console.log(shop.stock)
+console.log(shop.location)
+console.log(shop.status)
+console.log(shop.oprtn)
+//Defining Object book with five properties
+let book={category:"diary",year:"2020",pages:"100",covertype:"hard",source:"aristoc"}
+//Printing the 5 five properties of book object
+console.log(book.category)
+console.log(book.year)
+console.log(book.pages)
+console.log(book.covertype)
+console.log(book.source)
+//Defining Object crop with five properties
+let crop={name:"beans", family:"leguminaceae",origin:"Asia",height:"medium", seed:"dicot"}
+//Printing the 5 five properties of crop object
+console.log(crop.name)
+console.log(crop.family)
+console.log(crop.origin)
+console.log(crop.height)
+console.log(crop.seed)
+
