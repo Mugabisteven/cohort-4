55ff4d0e0187b3fb7514bc564437eb5bd43aaa96
Daniel-Tech4 <60209979+Daniel-Tech4@users.noreply.github.com>
2020-02-10 08:36:58 -0800

    My object oriented programming code.

---
+++ DanielokochaOOP.js
@@ -0,0 +1,42 @@
+// Declaring an object smartphone with ist five properties and there values.
+let smartphone={companyname:"Itel",model:"A14",ROM:"8GB",RAM:"512MB",buildNumber:"v049"}
+// Print all the five properties and values of the declaring object smartphone on the screen. 
+console.log(smartphone.companyname)
+console.log(smartphone.model)
+console.log(smartphone.buildNumber)
+console.log(smartphone.RAM)
+console.log(smartphone.ROM)
+// Declaring object bird with its five properties and there value.
+let bird={name:"chicken",wing:"two",leg:"two",peak:"one",eye:"two",}
+// Print all the declaring objects' properties  and values  on the screen.
+console.log(bird.name)
+console.log("Chicken's legs are" + bird.leg)
+console.log("Chicken eats with" + bird.peak)
+console.log(bird.eye)
+console.log(bird.wing)
+// Declaring object animal with five properties and there values. 
+let animal={Type:"wildAnimal",name:"lion",Legs:"four",body:"hairy",food:"only meat",}
+// Printing the object prperties and there values on the screen.
+console.log("Animal kindom has two types one is" + animal.Type)
+console.log(animal.name + " is a curnivorus animal.")
+console.log("Lion's legs is" + animal.Legs)
+console.log("The body is " + animal.body)
+console.log("It likes " + animal.food)
+// Declaring soil as an object with five properties and values.
+let soil={type:"loamsoil",color:"dark",layer:"top",uses:"for growing crops",weight:"40-40-20 ratio"}
+// Printing the object prperties and there values on the screen.
+console.log(soil.type + " is the best type of soil for crops growing.")
+console.log("The color of loam soil is " + soil.color)
+console.log("Loam soil is on the " + soil.layer)
+console.log("Loam soil is mostly use " + soil.uses)
+console.log(soil.weight + " is the actual weight of loam soil. ")
+// Declaring the object continent with its five properties and values.
+let continent={Name:"Africa",Region:"EastAfrica",country:"Uganda",CapitalCity:"Kampala",president:"Museveni",}
+// Print the declaring object properties and there values on the screen.
+console.log(continent.Name + " is a black continent.")
+console.log(continent.Region + " is one of the regions in Africa.")
+console.log(continent.country  + " ia an African country.")
+console.log(continent.CapitalCity + " is the capital city of Uganda.")
+console.log(continent.president + " is the president of the republic of Uganda.")
+
+
