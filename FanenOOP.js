721a1589c5bd45c3f7cdc717141837780af17d33
FanenFelix <60318855+FanenFelix@users.noreply.github.com>
2020-02-10 19:06:56 +0300

    Add files via upload
    Object with properties in Object Oriented programming

---
+++ FanenOOP.js
@@ -0,0 +1,47 @@
+ /*
+ Object Oriented programing in Javascript
+ */
+
+ //An object with a hospital value
+ let hospital = {name:"love clinic", ward:"female", location:"kampala", status:"24/7", size:"very big",}
+ //Display the hospital properties
+ console.log(hospital.name)
+ console.log(hospital.ward)
+ console.log(hospital.location)
+ console.log(hospital.status)
+ console.log(hospital.size)
+
+ //A church Object
+ let church = {name:"Deeper Life", location:"Kansanga", type:"Protestant", department:"Choir", section:"Youth section",}
+ //Properties of the object displayed
+ console.log(church.name)
+ console.log(church.location)
+ console.log(church.type)
+ console.log(church.department)
+ console.log(church.section)
+
+ //A market Object
+ let market ={name:"Owino", location:"Town", type:"Local market", vendors:"food and cloth vendors", }
+ //Display the market properties
+ console.log(market.name)
+ console.log(market.location)
+ console.log(market.type)
+ console.log(market.vendors)
+
+ //An object with a value "girl_friend"
+ let girl_friend ={name:"Edith", age:"22", location:"Kawenpe", appearance:"very beautiful", height:"very tall",}
+ //Display the properties of the object (girl_friend)
+ console.log(girl_friend.name)
+ console.log(girl_friend.age)
+ console.log(girl_friend.location)
+ console.log(girl_friend.appearance)
+ console.log(girl_friend.height)
+
+//Phones Object
+ let phones ={name:"Techno", brand:"CamonX", color:"Black", price:"1million", camera:"20px",}
+ //Displaying the values of the object (Phones)
+ console.log(phones.name)
+ console.log(phones.brand)
+ console.log(phones.color)
+ console.log(phones.price)
+ console.log(phones.camera)
