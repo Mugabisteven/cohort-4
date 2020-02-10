db916eb0ce129c89e8e2c4f4347d2a65d655ed9e
BatoAlvin <60175917+BatoAlvin@users.noreply.github.com>
2020-02-10 17:39:29 +0300

    Added objects and properties

---
+++ Alvinoop.js
@@ -0,0 +1,34 @@
+let movies = {Genre:"horror", Duration:"1hr 30mins",Ratings:5.3/10,Year:2019,Size:698} 
+      console.log(movies.Genre)
+      console.log(movies.Duration)
+      console.log(movies.Ratings)
+      console.log(movies.Year)
+      console.log(movies.Size)
+
+let dog = {Name:"Rex", Breed:"Mongolian",Age:5,Category:"khavsar",Appearance:"Bushy tail"} 
+      console.log(dog.Name)
+      console.log(dog.Breed)
+      console.log(dog.Age)
+      console.log(dog.Category)
+      console.log(dog.Appearance)
+
+let phone = {Storage:"8gb", Version:7.0,Language:"English",Devicename:"Tecnowx3P",Screenresolution:854*480} 
+      console.log(phone.Storage)
+      console.log(phone.Version)
+      console.log(phone.Language)
+      console.log(phone.Devicename)
+      console.log(phone.Screenresolution)
+
+let headphones = {Brand:"JBL", color:"white",weight:"0.05kg",Connection:"wireless",Size:"medium"} 
+      console.log(headphones.Brand)
+      console.log(headphones.color)
+      console.log(headphones.weight)
+      console.log(headphones.Connection)
+      console.log(headphones.Size)
+
+let bulb = {color:"white", wattage:"40amps",Diameter:"16mm",weight:"0.4kg",make:"China"} 
+      console.log(bulb.color)
+      console.log(bulb.wattage)
+      console.log(bulb.Diameter)
+      console.log(bulb.weight)
+      console.log(bulb.make)
