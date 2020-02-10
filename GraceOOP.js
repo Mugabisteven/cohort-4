33db269738b34038241e8304292146df582d64b6
Babirye Grace <41687107+babiryegrace20@users.noreply.github.com>
2020-02-10 19:42:27 +0300

    Add files via upload
    objects with their properties

---
+++ GraceOOP.js
@@ -0,0 +1,50 @@
+/**
+  Initialise the object bag with a list of properties
+  printing the values of the object properties
+ **/
+let bag = {size:"45", price:"2000UGX", category:"gucci" origin:"China", colour:"yellow"}
+console.log(bag.size)
+console.log(bag.price)
+console.log(bag.category)
+console.log(bag.origin)
+console.log(bag.colour)
+/**
+  Initialise the object courseunit with a list of properties
+  printing the values of the object properties
+ **/
+let courseunit = {name:"system admnistration", coursecode:"CSC2100", lecturer:"moses", creditunit:"3",course:"software engineering"}
+console.log(courseunit.name)
+console.log(courseunit.coursecode)
+console.log(courseunit.lecturer)
+console.log(courseunit.creditunit)
+console.log(courseunit.course)
+/**
+  Initialise the object phone with a list of properties
+  printing the values of the object properties
+ **/
+let phone = {price:"2000UGX", origin:"china", brand:"techno", RAM:"4GB", ROM:"16GB"}
+console.log(phone.price)
+console.log(phone.origin)
+console.log(phone.brand)
+console.log(phone.RAM)
+console.log(phone.ROM)
+/**
+  Initialise the object bank with a list of properties
+  printing the values of the object properties
+ **/
+let bank = {name:"stanbic",location:"jinja",owner:"pakash", manager:"oguttu",staffNumber:"50"}
+console.log(bank.name)
+console.log(bank.location)
+console.log(bank.owner)
+console.log(bank.manager)
+console.log(bank.staffNumber)
+/**
+  Initialise the object course with a list of properties
+  printing the values of the object properties
+ **/
+let course = {coursecode:"BSE245", name:"software engineering", lecturer:"mugabi", university:"makerere", courseunit:"5"}
+console.log(course.coursecode)
+console.log(course.name)
+console.log(course.lecturer)
+console.log(course.university)
+console.log(course.courseunit)
