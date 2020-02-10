971915e8aa393440dce7ebce57ff01245a4e9914
Odong Gilbert <47414230+gilbert-256@users.noreply.github.com>
2020-02-10 18:33:54 +0300

    Add files via upload
    Added five objects with five attributes each

---
+++ odongopp.js
@@ -0,0 +1,79 @@
+/**Declaring class person with five attributes*/
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age:20,
+    height:'160m',
+    weight:'68kgs'
+
+  };
+/**Printing out properties an object */
+
+ console.log(person.firstName)
+ console.log(person.lastName)
+ console.log(person.age)
+ console.log(person.height)
+ console.log(person.weight)
+
+/**Declaring class car with five attributes*/
+ let car={
+     make:'Ford',
+     model:'Mustang',
+     year:'1996',
+     engine:'wenkal',
+     tyre:'mud tyre'
+}
+/**Printing out properties an object */
+ console.log(car.make)
+ console.log(car.model)
+ console.log(car.year)
+ console.log(car.engine)
+ console.log(car.tyre)
+
+ /**Declaring class school with five attributes*/
+ let school = {
+    name : "Vivekananda School",
+    location : "Delhi",
+    established : "1971",
+    population:1459,
+    teacher:23,
+}
+/**Printing out properties an object */
+console.log(school.name)
+console.log(school.location)
+console.log(school.established)
+console.log(school.population)
+console.log(school.teacher)
+
+/**Declaring class dog with five attributes*/
+let dog = {
+Gestation_period: '58 days',
+Family: 'Canidae',
+Lifespan: '13 years',
+Order: 'Carnivora',
+Kingdom: 'Animalia'
+}
+
+/**Printing out properties an object */
+console.log(dog.Gestation_period)
+console.log(dog.Family)
+console.log(dog.Lifespan)
+console.log(dog.Order)
+console.log(dog.Kingdom)
+
+/**Declaring varriable and assigning attributes to it*/
+let house={
+    floor:'tiled',
+    windos:10,
+    doors:5,
+    roof:'iron_sheets',
+    power:'solar'
+}
+
+/**Printing out properties of an object */
+console.log(house.floor)
+console.log(house.windos)
+console.log(house.doors)
+console.log(house.roof)
+console.log(house.power)
+
