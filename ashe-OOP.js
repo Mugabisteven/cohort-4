29ed3228fc22f820a55f4d0f550d4de2bb03b049
ashe-senkatuuka <ashemukisa@gmail.com>
2020-02-10 15:40:27 +0300

    Added five javascript objects. Each object containing 5 properties with their values. Printed out to screen all object property values.

---
+++ ashe-OOP.js
@@ -0,0 +1,44 @@
+//Declaring object fruit with five properties and their values
+let fruit = {name:"Mango",color:"yellowish-orange", season:"perenial", nutrient:"vitamin A", juiceExtract:"true"}
+//Printing to screen all fruit object properties and their values
+console.log(fruit.name)
+console.log(fruit.color)
+console.log(fruit.season)
+console.log(fruit.nutrient)
+console.log(fruit.juiceExtract)
+
+//Declaring object aeroplane with five properties and their values
+let aeroplane = {name:"Airbus", manufacturer:"Boeing", country:"germany", seatingCapacity:800, fuelCapacity:"500L"}
+//Printing to screen all aeroplane object properties and their values
+console.log(aeroplane.name)
+console.log(aeroplane.manufacturer)
+console.log(aeroplane.country)
+console.log(aeroplane.seatingCapacity)
+console.log(aeroplane.fuelCapacity)
+
+//Declaring object planet with five properties and their values
+let planet = {name:"Mars", distanceFromSun:"50000km", humanLife:"false", color:"red", moons:2}
+//Printing to screen all planet object properties and their values
+console.log(planet.name)
+console.log(planet.distanceFromSun)
+console.log(planet.humanLife)
+console.log(planet.color)
+console.log(planet.moons)
+
+//Declaring object civilization with five properties and their values
+let civilization = {name:"Stone Age", primaryTool:"Stone", worldPopulation:100000, year:"4000BC", habitation:"caves"}
+//Printing to screen all civilization object properties and their values
+console.log(civilization.name)
+console.log(civilization.primaryTool)
+console.log(civilization.worldPopulation)
+console.log(civilization.year)
+console.log(civilization.habitation)
+
+//Declaring object music with five properties and their values
+let music = {genre:"reggae", intruments:"guitar, bass, drums", countryOfOrigin:"Jamaica", famousArtist:"Bob Marley", yearOfInseption:1970}
+//Printing to screen all music object properties and their values
+console.log(music.genre)
+console.log(music.intruments)
+console.log(music.countryOfOrigin)
+console.log(music.famousArtist)
+console.log(music.yearOfInseption)
