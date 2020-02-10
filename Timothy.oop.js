6109906c3d8898674d185f5fe7eccb7493701e26
Timothy-sserunjogi <60179346+Timothy-sserunjogi@users.noreply.github.com>
2020-02-10 19:39:31 +0300

    Five Objectives with five properties

---
+++ Timothy.oop.js
@@ -0,0 +1,44 @@
+/** An object defined as job which is assigned to different requirements*/
+let job = {name: "Accountant", type: "finance", experience: "2yrs", level: "juniorAcc", pay: "1200000"}
+/**Printing the output of job*/
+console.log("A company needs an :" + job.name)
+console.log("In the finance sector:" + job.type)
+console.log("Who have technical experience of:" + job.experience)
+console.log("And is relatively a:" + job.level)
+console.log("Who's salary is :" + job.pay)
+
+/**Defining an object as melon which is assigned to the melon categories */
+let melon = {name: "cantaloupes", color: "grayishgreen", biggestgrowth: "NorthAmerica", vitamincontent: "A&C", locationgrown: "Canada"}
+/**Printing the output of melon */
+console.log("there are many types of melons such as:"+melon.name)
+console.log("it is of a color:" +melon.color)
+console.log("the cantaloupe is highly grown in:"+ melon.biggestgrowth)
+console.log("we get vitamin:" + melon.vitamincontent)
+console.log("You can as well find it grown in:"+ melon.locationgrown)
+
+/**An object defined as chocolate */
+let chocolate = {brandname: "toblerone", countryorigin: "Switzerland", yearbegun: "1908", shape: "triangular", specialingredient: "almonds"}
+/**printing out the chocolate properties */
+console.log("Chocolate like:" + chocolate.brandname)
+console.log("Is produced in:"+chocolate.countryorigin)
+console.log("Toblerone manufacturing began in:" +chocolate.yearbegun)
+console.log("It is:"+ chocolate.shape)
+console.log("Toblerone is also made of:" + chocolate.specialingredient)
+
+/**Defining an object as richestkid which is being assigned to different properties */
+let richestkid = {kidsname: "Prince George Alexaner Loius", networth: "$1billion", age: "6yrs", knownfact: "Comes from the Royal British family", countryoforigin: "United Kingdom"}
+/**printing out the properties of the object richestkid */
+console.log("The richest kid in the world is:" + richestkid.kidsname)
+console.log("His networth is:" + richestkid.networth)
+console.log("Can you imagine he is :" + richestkid.age)
+console.log("An amazing fact is that he:" + richestkid.knownfact)
+console.log("He originates from:" + richestkid.homecountry)
+
+/**Defining an object as technologies */
+let technologies = {company: "AppleInc", Hq: "Califonia_USA", founder: "SteveJobs", CEO: "TimCook", product: "Iphone"}
+/**printing the properties of technologies */
+console.log("One of the big four tech companies in the world is :" + technologies.company)
+console.log( "Apple's Hq are in :" + technologies.Hq)
+console.log("The mind behind the existance of Apple is :" + technologies.founder)
+console.log("The CEO of Apple is :" + technologies.CEO)
+console.log("Apple makes products like :" + technologies.product)
