a302c28e6e7b1ae6569eafb5922e2e3307915d81
briantical <lutbrianivan@gmail.com>
2020-02-10 17:24:47 +0300

    Added Brian's OOP work

---
+++ BrianOOP.js
@@ -0,0 +1,98 @@
+/**
+ * A test of Object Oriented programming with javascript
+ */
+
+//A movies object
+let movie = {
+  title: "Avengers: Endgame",
+  studio: "Marvel Studios",
+  actors: "Robert Downey Junior, Josh Brolin, Chris Evans",
+  rating: 4.9,
+  release_year: 2018
+};
+
+//Display the movie properties
+console.log("The movies object");
+console.log("=================");
+console.log("The movie's title is: " + movie.title);
+console.log("The movie's studio is: " + movie.studio);
+console.log("The movie's actors are: " + movie.actors);
+console.log("The movie's rating is: " + movie.rating);
+console.log("The movie's release year is: " + movie.release_year);
+console.log(" ");
+
+//A songs object
+let song = {
+  title: "The Box",
+  artiste: "Roddy Ricch",
+  genre: "unknown",
+  release: "December 6, 2019",
+  album: "Please Excuse Me for Being Antisocial"
+};
+
+//Display the songs properties
+console.log("The songs object");
+console.log("=================");
+console.log("The song's title is: " + song.title);
+console.log("The song's artiste is: " + song.artiste);
+console.log("The song's genre is: " + song.genre);
+console.log("The song's release date is: " + song.release);
+console.log("The song's album is: " + song.album);
+console.log(" ");
+
+//Tribes Object
+let tribe = {
+  name: "Baganda",
+  ethinicity: "Bantu",
+  origin: "Adamawa Highlands, Cameroon",
+  languages: "Luganda",
+  foods: "Matooke"
+};
+
+//Display the tribes properties
+console.log("The tribes object");
+console.log("=================");
+console.log("The tribe's name is: " + tribe.name);
+console.log("The tribe's ethinicity is: " + tribe.ethinicity);
+console.log("The tribe's origin is: " + tribe.origin);
+console.log("The tribe's languages is: " + tribe.languages);
+console.log("The tribe's foods is: " + tribe.foods);
+console.log(" ");
+
+//Smartphones object
+let smartphone = {
+  name: "IPhone XR",
+  brand: "Apple",
+  release: "2018",
+  cost: 1200,
+  production: true
+};
+
+//Display the tribes properties
+console.log("The smartphones object");
+console.log("=================");
+console.log("The smartphone's name is: " + smartphone.name);
+console.log("The smartphone's brand is: " + smartphone.brand);
+console.log("The smartphone's release is: " + smartphone.release);
+console.log("The smartphone's cost is: " + smartphone.cost);
+console.log("Is the smartphone in production: " + smartphone.production);
+console.log(" ");
+
+//A Bank object
+let bank = {
+  name: "ABSA Bank Uganda",
+  registration_number: 442123,
+  type: "Private Limited",
+  employees: 1200,
+  year: 2018
+};
+
+//Display the tribes properties
+console.log("The Banks object");
+console.log("=================");
+console.log("The bank's name is: " + bank.name);
+console.log("The bank's registration number is: " + bank.registration_number);
+console.log("The bank's company type is: " + bank.type);
+console.log("The bank's total number of employees is: " + bank.employees);
+console.log("The bank's year of incorporation is: " + bank.year);
+console.log(" ");
