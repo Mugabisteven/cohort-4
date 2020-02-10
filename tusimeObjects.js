2cdc9ea59f158397485d8e25efdbfedc545191b8
tusimegod123 <43960999+tusimegod123@users.noreply.github.com>
2020-02-10 18:28:55 +0300

    First assignment onObjects

---
+++ tusimeObjects.js
@@ -0,0 +1,66 @@
+/*Defining the relationship method and asigning it with different individual's statuses*/
+let relationship ={Mary:"Divorced", Jane:"Married", Godwin:"Single", Yusuf:"Complected", Edith:"Not sure"}
+//Printing the outcomes of the relationship object on the console
+console.log("Mary is "+ relationship.Mary)
+console.log("Jane is Happily "+ relationship.Jane)
+console.log("Godwin is misarably "+ relationship.Godwin)
+console.log("Yusuf's ralationship is "+ relationship.Yusuf)
+console.log("For edith she is "+ relationship.Edith)
+
+/*Defining the radio object showing the different types and where they are manufactured from*/
+let radio = {Sony:"China",Panasonic:"Thaiwan",Makula:"Uganda",LG:"England",Lasonic:"Japan"}
+
+//This console.log function creates space between relationship and radio object statements on the console
+console.log()
+
+//Printing the results of the radio object on the console
+console.log("Sony radios are made from "+ radio.Sony)
+console.log("panasonic radios are made from "+radio.Panasonic)
+console.log("Makula radios are made from "+ radio.Makula)
+console.log("LG radios are made from "+ radio.LG)
+console.log("Lasaonic Radios are made from "+radio.Lasonic)
+
+/* Defining money as an object and asigning it to different countries in terms of currency*/
+let money = {USA:"Dollars",Uganda:"Shillings",England:"Pounds",Nigeria:"Naira",Kenya:"KSH"}
+
+//This console.log function creates space between the radio and premier league object statements on the console
+console.log()
+
+//Printing the results of money object on the console
+console.log("USA uses "+ money.USA +"as their currency")
+console.log("Uganda uses  "+money.Uganda +"as their currency")
+console.log("England uses"+ money.England + "as their currency")
+console.log("Nigeria uses "+ money.Nigeria +"as their currency")
+console.log("Kenya uses "+money.Kenya +"as their currency")
+
+/* Defining premier league as an object and showing what one needs to know about it briefly */
+let premier_league = {clubs:20,players:30,stadiums:20,matches:380,Location:"England"}
+
+//This console.log function creates space between premier league and libray object statemnts on the console
+console.log()
+
+//printing the results of the premier league objects on the console
+console.log("The Premier League has "+premier_league.clubs +"teams or clubs")
+console.log("Each premier league team has "+premier_league.players+"players")
+console.log("It has "+ premier_league.stadiums +"stadiums")
+console.log("All the league games played totals to "+ premier_league.matches +"matches")
+console.log("It is found in a country called "+premier_league.Location)
+
+/* Defining a library object that stores details what can be expected of a library */
+let library = {identificatio:"ID_number",reading_materials:"Books",Furniture:"chairs and tables",network:"Wifi",attendant:"libralian"}
+
+//This console.log function creates space between premier league and libray object statemnts on the console
+console.log()
+
+//Now Printing the results of library objects on the console
+console.log("To use a library one needs a "+ library.identificatio)
+console.log("materials you can find in the library are "+library.reading_materials)
+console.log("People use "+ library.Furniture +"to seat and read from in the library")
+console.log("To access internet in the library users use "+ library.network)
+console.log("A person who attends to library users is a "+library.attendant)
+
+
+
+
+
+
