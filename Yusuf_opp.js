f61d351e7667395d818be8d57ce855f494dd3a27
Yusuf Mugagga Kasajja <57800939+mugagga-kasajja@users.noreply.github.com>
2020-02-11 01:19:46 +0300

    Add files via upload
    first object oriented programming assignment

---
+++ Yusuf_opp.js
@@ -0,0 +1,65 @@
+/**Declaring different objects that have differennt properties each,
+ * these properties have been given different values accordingly. **/
+ let favourite_song={
+    Title: "Heaven Sent",
+    Artist: "Keyshia Cole",
+    genre: "RnB",
+    Year: "2009"
+}
+/**Printing all the different property values */
+console.log(favourite_song.Title)
+console.log(favourite_song.Artist)
+console.log(favourite_song.genre)
+console.log(favourite_song.Year)
+
+let girlfriend={
+  Name: "Nassuna Joan",
+  DOB: "15th Febuary 1997",
+  star_sign:"Aquarius",
+  waist_size:"30 inch",
+  height:"5'7",
+  weight:"66kg"
+}
+console.log(girlfriend.Name)
+console.log(girlfriend.DOB)
+console.log(girlfriend.star_sign)
+console.log(girlfriend.waist_size)
+console.log(girlfriend.height)
+console.log(girlfriend.weight)
+
+let gym_programme={
+  Day:"Tuesday",
+  Programme:"Shoulders Day",
+  workout:"Arnold press",
+  muscle_targeted:"Front Shoulders",
+  sets:"5",
+  reps:"20"
+}
+console.log(gym_programme.Day)
+console.log(gym_programme.Programme)
+console.log(gym_programme.workout)
+console.log(gym_programme.muscle_targeted)
+console.log(gym_programme.sets)
+console.log(gym_programme.reps)
+
+let best_team={
+  Team:"Manchester United",
+  Started_in:"1878",
+  Num_of_trophies:"66",
+  current_position:"7th"
+}
+console.log(best_team.Team)
+console.log(best_team.Started_in)
+console.log(best_team.Num_of_trophies)
+console.log(best_team.current_position)
+
+let Quran={
+  Surah:"Al-Baqara",
+  Meaning:"The Cow",
+  num_of_Verses:"286",
+  position:"second"
+}
+console.log(Quran.Surah)
+console.log(Quran.Meaning)
+console.log(Quran.num_of_Verses)
+console.log(Quran.position)
