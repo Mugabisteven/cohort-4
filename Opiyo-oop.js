ac33c8b6b324596fce37d31331c5a261641a9486
chrisopiyo <60352290+chrisopiyo@users.noreply.github.com>
2020-02-10 05:55:11 -0800

    Opiyo

---
+++ Opiyo-oop.js
@@ -0,0 +1,74 @@
+let city = {
+    name:"New york",
+    location:"USA",
+    continent:"North America",
+    landArea:"783.8sq km", 
+    population:"8.5million people"
+}
+console.log( city.name)
+console.log(city.location)
+console.log(city.continent)
+console.log(city.landArea)
+console.log(city.population)
+console.log("----------------------------")
+
+let techschool = {
+    name:"refactory",
+    District:"kampala ",
+    village:"muyenga",
+    address:"3rd floor, Le palm building,Lubobbo close",
+    program:"ctalyst,bootcamp"
+}
+console.log(techschool.name)
+console.log(techschool.District)
+console.log(techschool.village)
+console.log(techschool.address)
+console.log(techschool.program)
+console.log("------------------")
+
+
+let twinbro = {
+    name:"Giovanna",
+    residence:"milan",
+    hobbies:"ski",
+    favoriteseason:"winter",
+    allergy:"okra"
+}
+console.log(twinbro.name)
+console.log(twinbro.residence)
+console.log(twinbro.hobbies)
+console.log(twinbro.favoriteseason)
+console.log(twinbro.allergy)
+console.log("---------------------")
+
+
+let soccerclub = {
+    name:"manchester united",
+    country:"England",
+    Teamcoach:"Ole Gunnar solskjaer",
+    BestGoalie:"David de Gea Quintana",
+    NewSignings:"Bruno fernandes,Odion Ighalo"
+}
+console.log(soccerclub.name)
+console.log(soccerclub.country)
+console.log(soccerclub.Teamcoach)
+console.log(soccerclub.BestGoalie)
+console.log(soccerclub.NewSignings)
+console.log("-------------------")
+
+
+let mydog = {
+    name:"Neymar",
+    Breed:"cross",
+    Color:"brown",
+    Purpose:"Pet",
+    Skills:"Opening gate"
+}
+console.log(mydog.name)
+console.log(mydog.Breed)
+console.log(mydog.Color)
+console.log(mydog.Purpose)
+console.log(mydog.Skills)
+
+
+
