21e3fdc72ec8ab170ae142ca5714fe116e53f162
Dianah-code <60173686+Dianah-code@users.noreply.github.com>
2020-02-10 14:04:39 +0300

    Add files via upload

--- Danah.js
+++ Danah.js
@@ -1,60 +1,59 @@
-//user defined function named tests defines paramter named test1 & test2 and is enclosed in parentheses
+//Define function tests that takes 2 parameters
 function tests(test1,test2)
-//code to be executed is placed inside curly brackets
 {
-//Conditional operator used to define statement if test1 >test1 assign test1 or else test 2
+//Declare variable greater and assign to result of the greater of test1 or test 2
 let greater = test1>test1?test1:test2
-//return passes a value from function greater back to the code that called it but function stops executing as soon as it reaches return
+//Returns the greater value of the 2 parameters to any calling functions
 return greater
 
 }
-//function named coursework defines parameter named cswork
+//Define function coursework that takes one parameter
  function coursework(cswork)
  {
-//declare and innitialise variable coursework adding values.
-//tests (80,80) is returned value from greater
+//Declare variable coursework and assign it to summation of value parameter cswork and returning value of function tests
  let coursework = cswork + tests(80,80)
- //print result of variable coursework defined prior
+ //Print result of variable coursework 
  console.log(coursework)
-//return passes a value from function coursework back to the code that called it but function stops executing as soon as it reaches return
+//Returns the value of the function coursework to any calling functions
 return coursework
 
  }
-//function named avg defines parameter named a
+//Define a function named avg to one parameter
 function avg(a)
 {
-//declare and innitialise variable avg by dividing the product of recalled value of function coursework with 90 by value a .
+//Declare variable avg to the returning value of function coursework divided by parameter a
 let avg =coursework(90)/a
-//return passes a value from a function avg to the code that called it but function stops executing as soon as it reaches return
+//Returns the value of function avg to any calling functions
 return avg
 
 }
 
-//function named crsmark defines parameter named a
+//Define function crsmark and takes one parameter
 function crsmark(a)
 {
-//declare and innitialise variable exm with multplcation of recalled values of function avg with 2 and diviging value a by 100.
+//Declare variable exm and assign to the product of returning value of function avg and percentage of parameter a 
 let exm = avg(2)*(a/100)
-//return passes a value from a function exm to the code that called it but function stops executing as soon as it reaches return
+//Returns the value of variable exm to any calling functions
  return exm 
 }
-//function named exam defines recalled value to parameter named a
+//Define function exam and takes one parameter 
 function exam(a)
 {
-//declare and innitialise variable fexam with multplcation of fraction 60/100 with recalled values of function a   
+//Declare and innitialise variable fexam to product of 60 percentage of value of parameter a
 let fexam = (60/100)*a
-//return passes a value from a function fexm to the code that called it but function stops executing as soon as it reaches return
+//Returns the value of variable fexm to any calling functions
 return fexam
 
 }
 //Empty brackets define an anoymous self executing function  fnal and then calling it with no argments effectively causing the block to be evaluated
 function fnal()
 {
- //declare and innitialise variable fmark addtion of product of value exam with 75 and product of value crsmark with 40
+ //Declare and innitialise variable fmark addtion of product of value exam with 75 and product of value crsmark with 40
 let fmark = exam(75)+crsmark(40)
-//command to print results of fmark
+//Command to print results of fmark
 console.log(fmark)
 
 }
-//Provides scoping for executing fnal function so it doesn't pollute the global JS name space. Functions outside can't be used
+//Provides scoping for executing fnal function 
 fnal()
+//Calls fnal function that interlinks the rest
