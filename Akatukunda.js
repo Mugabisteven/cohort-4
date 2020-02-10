e4b7695c15f527924e7b6a8b0da111761230ddad
akapesh <60213676+akapesh@users.noreply.github.com>
2020-02-10 19:27:29 +0300

    interprete defining changes

--- Akatukunda.js
+++ Akatukunda.js
@@ -3,14 +3,14 @@ function tests(test1,test2)
 {
 //Check if test2 is > test1 -if true take test1 else take test2
 let greater = test2>test1?test1:test2
-//Returns the value in the greater memory space
+//Returns greater to any calling function
 return greater
 }
 
 //Defining the function coursework
 function coursework(cswork)
  {
- //Coursework assignment & tests invocation
+ //Coursework assignment & tests function invocation
  let coursework = cswork + tests(80,80)
  //Prints out coursework
  console.log(coursework)
@@ -21,18 +21,21 @@ return coursework
 //Defining the function avg
 function avg(a)
 {
-//Coursework invocation which is divisble by avg	
+/**
+Avg assignment 
+Coursework function invocation which is divisble by avg	
+**/
 let avg =coursework(90)/a
-//Returns avg
+//Returns avg to any calling function
 return avg
 }
 
 //Defining the function crsmark
 function crsmark(a)
 {
-//Exm assignment & avg invocation & un unknown parameter
+//Exm assignment & avg function invocation & un unknown parameter
 let exm = avg(2)*(a/100)
-//Returns exm
+//Returns exm to the calling function
 return exm 
 }
 
@@ -41,14 +44,14 @@ function exam(a)
 {
 //Fexam assignment & un known parameter
 let fexam = (60/100)*a
-//Returns fexam
+//Returns fexam to the calling function
 return fexam
 }
 
 //Defining the function fnal
 function fnal()
 {
-//Fmark assignment & invocation of exam & crsmark parameters
+//Fmark assignment & invocation of functions exam & crsmark parameters
 let fmark = exam(75)+crsmark(40)
 //Prints out fmark
 console.log(fmark)
@@ -56,6 +59,7 @@ console.log(fmark)
 //Calling the function fnal
 fnal()
 
-//This program computes the final marks of a student using variables like coursework, exam and then 
-//the total is displayed in the variable name fmark
-
+/**
+This program computes the final marks of a student using variables like coursework, exam and then 
+the total is displayed in the variable name fmark
+**/
