cb05bee12e809e4ae86ac31e070dc724d7d1ee56
Mugabi Steven <60210804+Mugabisteven@users.noreply.github.com>
2020-02-10 18:05:50 +0300

    Uploaded Mugabi's file

---
+++ MugabiOOP.js
@@ -0,0 +1,42 @@
+//Defining beverages class sodas with 5 classes.
+let soda={pepsi:"mirinda",cocacola:"fanta",riham:"rockboom",caanan:"skyview",wavah:"water"}
+//Printing out properties of the object.
+console.log(soda.pepsi)
+console.log(soda.cocacola)
+console.log(soda.riham)
+console.log(soda.caanan)
+console.log(soda.wavah)
+
+//Defining class alcahol of different brands with 5 classes.
+let alcahol={ubl:"tuskerlite",nilebrewies:"nilestount",Heineken:"heinekenlager",whiskey:"mamont",vodka:"absolute"}
+//Printing out properties of the object.
+console.log(alcahol.ubl)
+console.log(alcahol.nilebrewies)
+console.log(alcahol.Heineken)
+console.log(alcahol.whiskey)
+console.log(alcahol.vodka)
+
+//Defining building materials with 5 classes
+let buildingmaterials={cement:"hima",nails:"roofings",paint:"sadolin",ironbars:"sembule",tiles:"goodwill"}
+//Printing out properties of the object.
+console.log(buildingmaterials.cement)
+console.log(buildingmaterials.nails)
+console.log(buildingmaterials.paint)
+console.log(buildingmaterials.ironbars)
+console.log(buildingmaterials.tiles)
+//Defining company's bookings with 5 classes
+let bookings={hotels:"mestil",leisureparks:"adveturepark",safaris:"greatlakes",gameparks:"murchusion",resorts:"wildwater"}
+//Printing out properties of the object.
+console.log(bookings.hotels)
+console.log(bookings.leisureparks)
+console.log(bookings.safaris)
+console.log(bookings.gameparks)
+console.log(bookings.resorts)
+//Defining the academedia categories by 5 classes.
+let academia={kinder:"littletots",pschool:"kps",secondary:"smack",tertiary:"ymca",university:"ucu"}
+//Printing out properties of the object.
+console.log(academia.kinder)
+console.log(academia.pschool)
+console.log(academia.secondary)
+console.log(academia.tertiary)
+console.log(academia.university)
