816189d068b02cefae8e9bdb0ca1f29aef8412ab
PatienceKunihira <60210440+PatienceKunihira@users.noreply.github.com>
2020-02-10 19:24:56 +0300

    Added objects and properties

---
+++ FridahOOP.js
@@ -0,0 +1,96 @@
+/**
+Defining object hair and assigning it to the diffrent properties.      
+**/
+ let hair ={
+ 	style:"braids",
+ 	brand: "darling ",
+ 	length:"long", 
+ 	colour:"black",
+ 	texture:"hard"
+ 			}
+/**
+Print out the hair properties      
+**/
+ console.log("The name of the hair style is:" +hair.style)
+ console.log("The name of the hair brand is:" +hair.brand)
+ console.log("The hair length is:" +hair.length)
+console.log("The name of the hair colour is:" +hair.colour)
+console.log("The hair texture is:" +hair.texture)
+/**
+Defining object cookies and assigning  it to the different properties 
+**/
+ var cookies ={
+ 	company:"Bake for me",
+ 	flavour:"coconut_cookies",
+ 	packing:"containers",
+ 	shape:"rectangular,circular,triangular",
+ 	origin:"Uganda"
+
+
+ 				}
+
+ //Print the cookies properties 				
+ console.log("This is the name of the bakery:"+cookies.company)
+ console.log("We make:"+cookies.flavour)
+console.log("They are packaged in:"+cookies.packing)
+console.log("The different shapes are:"+cookies.shape)
+console.log("They are made in:"+cookies.origin)
+
+
+/**
+Defining object shoes and assigning it to the diffrent properties.      
+**/
+ let shoes ={
+	type:"Forever 21",
+	size:"37", 
+	material:"leather" ,
+	price:"35000" ,
+	origin:"Califonia"
+			}
+/**
+Print the shoes properties.      
+**/			
+ console.log( "Pesh wears: "+shoes.type + " shoe type")
+console.log("Her shoe size is :"+shoes.size)
+console.log("The shoe material is:"+shoes.material)
+console.log("The shoe price is:"+shoes.price)
+console.log("It was made in:"+shoes.origin)
+
+/**
+Defining object airlines and assigning it to the diffrent properties.      
+**/
+let airlines ={
+	name:"Qatar", 
+	capacity:"233",
+	origin:"Doha_Qatar",
+	model:"Boeing 747", 
+	destinations:"172"
+ 				}
+ 				/**
+ 				Print out the airlines properties
+      **/
+ console.log("The airlines name is :"+airlines.name)
+ console.log(" he capacity is:"+airlines.capacity)
+console.log("They are made in:"+airlines.origin)
+console.log("The model is:"+airlines.model)
+ console.log("The destinations are over:"+airlines.destinations)
+
+/**
+Defining object beverages and assigning it to the diffrent properties.      
+**/
+
+ let beverages={
+ 	name:"Nile stout",
+ 	manufacturing_company:"Uganda_breweries",
+ 	 type:"beer",
+ 	  bottle_size:"250ml",
+ 	  price:"5000"
+ 				}
+ /**
+Print the beverages properties    
+**/
+ console.log("The beverage name is:"+beverages.name)
+ console.log("It was manufactured from :"+beverages.manufacturing_company)
+ console.log("The beverage is a :"+beverages.type)
+ console.log("The bottle size is:"+beverages.bottle_size)
+ console.log("A bottle of beer is :"+beverages.price)
