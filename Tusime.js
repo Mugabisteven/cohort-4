efa9652ddedd0d1365263b4f820325a1f9ab8550
tusimegod123 <43960999+tusimegod123@users.noreply.github.com>
2020-02-10 10:59:01 +0300

    Update Tusime.js

--- Tusime.js
+++ Tusime.js
@@ -1,4 +1,4 @@
-   // Declaring the tests function
+   // Defining the tests function
    function tests(test1,test2)
 {
     // Asigning the greater test mark to greater
@@ -6,7 +6,7 @@
 // Stores a greater test results
    return greater 
 }
-//Declaring the Coursework function
+//Defining the Coursework function
  function coursework(cswork) 
  {
      //Asigning the cswrk plus the test mark to coursework
@@ -19,18 +19,18 @@
 return coursework
 
  }
-// Declaring the avg function
+// Defining the avg function
 function avg(a)
 {
     //Asigning the 90 mark to coursework which will be added to the greatest mark(80) divided by 2
 let avg =coursework(90)/a
 
-//stores the avg results
+//Stores the avg results
 return avg
 
 }
 
-//Declaring the crsmark function
+//Defining the crsmark function
 function crsmark(a)
 {
     // Asigning 2 to avg function and multiply it by crsmark function 
@@ -38,17 +38,17 @@ let exm = avg(2)*(a/100)
 //stores the exm mark
  return exm 
 }
-//decalaring the exam function
+//Defining the exam function
 function exam(a)
 {
-    //asigning 60/100 * a which is the exam function 
+    //Asigning 60/100 * a which is the exam function 
 let fexam = (60/100)*a
 
-//stores the fexam results
+//Stores the fexam results
 return fexam
 
 }
-//Declares the fnal function
+//Defining the fnal function
 function fnal()
 {
     //Asigning the sum of exam function and crsmark function to fmark
@@ -58,7 +58,7 @@ let fmark = exam(75)+crsmark(40)
 console.log(fmark)
 
 }
-//invokes the fnal function
+//Invokes the fnal function
 fnal()
 
 // The above program calculates and prints the reults of course work function(cswork) which is 170 as aresult of adding 90 to 80
