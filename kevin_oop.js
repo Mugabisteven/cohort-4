e34fc07d71931f9e12fdd6741bf7fddc2fa50cf8
Otimkev <56061354+Otimkev@users.noreply.github.com>
2020-02-10 12:52:36 +0000

    OOP example uploaded

---
+++ kevin_oop.js
@@ -0,0 +1,15 @@
+/*Defining a class Laptops with five objects
+with five properties*/
+Laptops = {
+    name:"dell",
+    hdd_capcity:"500Gb",
+    Ram:"12Gb",
+    video_graphics:"2Gb",
+    color:"black"
+}
+console.log(Laptops.name)
+console.log(Laptops.hdd_capcity)
+console.log(Laptops.ram)
+console.log(Laptops.video_graphics)
+console.log(Laptops.color)
+
