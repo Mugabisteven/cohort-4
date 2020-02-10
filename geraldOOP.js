08460f12b341535df5a0eac2f39d52768eecbe48
Gerald Ssali <57569353+gs-maker@users.noreply.github.com>
2020-02-10 17:53:14 +0300

    Objects and Parameters assignment

---
+++ geraldOOP.js
@@ -0,0 +1,48 @@
+// Declaring object "house"
+let house = { type: "Bangalow", address: "Bukoto", street: "Butoto_Street", numberOfRooms: "Five", roofStyle: "flat" }
+// Printing "house" object values and their properties
+console.log("the type of house is: " + house.type)
+console.log("the address of house is: " + house.address)
+console.log("the street of house is: " + house.street)
+console.log("the number of rooms in the house is: " + house.numberOfRooms)
+console.log("the style of the roof is: " + house.roofStyle)
+
+
+// Declaring object "underwear"
+let underwear = { type: "thong", material: "silk", color: "pink", style: "lace", size: "extraLarge" }
+// Printing "underwear" object values and their properties
+console.log(underwear.type)
+console.log(underwear.material)
+console.log(underwear.color)
+console.log(underwear.style)
+console.log(underwear.size)
+
+
+// Declaring object "lion"
+let lion = { kingdom: "animalia", order: "carnivora", skinType: "fur", prey: "antelope", topSpeed: "56kph" }
+// Printing "lion" object values and their properties
+console.log("the kingdom for lion is: " + lion.kingdom)
+console.log("the order is: " + lion.order)
+console.log("the lion's skin type is: " + lion.skinType)
+console.log("the lion preys on: " + lion.prey)
+console.log("it has a top speed of: " + lion.topSpeed)
+
+
+// Declaring object "plumbing"
+let plumbing = { type: "faucet", purpose: "controlRelease", material: "copper", diameter: "120mm", sealType: "rubber" }
+// Printing "plumbing" object values and their properties
+console.log(plumbing.type)
+console.log(plumbing.purpose)
+console.log(plumbing.material)
+console.log(plumbing.diameter)
+console.log(plumbing.sealType)
+
+
+// Declaring object "calendar"
+let calendar = { type: "tactile", material: "paper", variant: "wallCalendar", purpose: "reminder", maxLength: "12months" }
+// Printing "calendar" object values and their properties
+console.log(calendar.type)
+console.log(calendar.material)
+console.log(calendar.variant)
+console.log(calendar.purpose)
+console.log(calendar.maxLength)
