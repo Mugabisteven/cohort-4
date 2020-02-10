cca1bdbb9f9f566ecae6dba9e35cd006e56d402c
akapesh <60213676+akapesh@users.noreply.github.com>
2020-02-10 19:29:42 +0300

    object definition 1st commit

---
+++ AkatukundaOOP.js
@@ -0,0 +1,67 @@
+/**
+Defines object patience 
+Assignment of respective properties
+**/
+let patience = {Nationality: "Ugandan", Age: 26, Height: "160cm", HairColor: "Black", ShoeSize: 38} 
+
+//Prints out patience's properties
+console.log("Patience is " + patience.Nationality)
+console.log("She is " + patience.Age)
+console.log("She is " + patience.Height + " tall")
+console.log("Her haircolor is " + patience.HairColor)
+console.log("Her shoesize is " + patience.ShoeSize)
+
+/**
+Defines object university 
+Assignment of respective properties
+**/
+let university = {Name: "MUBS", Country: "Uganda", Location: "Nakawa", Speciality: "Business", Ranking: "3rd position"}
+
+//Prints out university properties
+console.log("The name of the university is " + university.Name)
+console.log("It is found in " + university.Country)
+console.log("Located in " + university.Location)
+console.log("The university specializes in " + university.Speciality)
+console.log("It holds a " + university.Ranking + " ranking in East Africa")
+
+/**
+Defines object music 
+Assignment of respective properties
+**/
+let music = {Artist: "Pompi", Born: "Zambia", Genre: "Gospel/Christian", Albums: "Broken English, Mizu, Become",
+Recordlabels: "Atmosphere Nation, Times Music, Atmosphere Enterprise, Lota House"}
+
+//Prints out music properties
+console.log("Artist name is " + music.Artist)
+console.log("He is of " + music.Born + " origin")
+console.log("He is a " + music.Genre + " Artist")
+console.log("Pompi has released these albums: " + music.Albums)
+console.log("Pompi has signed with these labels: " + music.Recordlabels)
+
+/**
+Defines object airline 
+Assignment of respective properties
+**/
+let airline = {Name: "UgandaAirlines", HQLocation: "Entebbe", OrganisationType: "Flag Carrier", 
+Hub: "Entebbe Internatinal Airport", Founded: "January, 30th, 2018"}
+
+//Prints out airline properties
+console.log("The Airline name is " + airline.Name)
+console.log("The Airline Head Quarters is located in " + airline.HQLocation)
+console.log("Uganda Airlines is a " + airline.OrganisationType)
+console.log("The Airline hub is " + airline.Hub)
+console.log("The Airline was founded on " + airline.Founded)
+
+/**
+Defines object clubs 
+Assignment of respective properties
+**/
+let clubs = {Name: "Arsenal FC", Nickname: "The Gunners", Manager: "Mikel Arteta", Stadium: "Emirates Stadium", 
+YearFormed: 1886}
+
+//Prints out clubs's properties
+console.log("The club name is " +  clubs.Name)
+console.log("Arsenal's nickname is: " +  clubs.Nickname)
+console.log("The club is managed by " +  clubs.Manager)
+console.log("The club plays in the " +  clubs.Stadium)
+console.log("Arsenal FC was formed in the year " +  clubs.YearFormed)
