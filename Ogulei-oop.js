4d5dc277e1006e16aa65d6568a79dc1b8c014c84
Daniel-2012 <60190253+Daniel-2012@users.noreply.github.com>
2020-02-10 10:27:24 -0500

    Add files via upload

--- Ogulei-oop.js
+++ Ogulei-oop.js
@@ -1,3 +1,35 @@
-let person={ name:"dan", age: 22,height:1.5}
-console.log(person.name)
-console.log(person.age)
+let animal={pet:"cat", aquatic:"fish", biggest:"elephant", flying:"bat", pouched:"wallaby"}
+console.log("this is a "+ animal.pet + ".")
+console.log("A " + animal.aquatic + ".")
+console.log("The biggest animal in the bush is an " + animal.biggest + ".")
+console.log("The only flying mammal is a " + animal.flying + ".")
+console.log("A " + animal.pouched + "; the creatcher with a pouch.") 
+
+let light={sky1:"sun", sky2:"moon", sky3:"stars", art:"lump", glow:"worm" }
+console.log("The main source of light is the "+ light.sky1 +".")
+console.log("The " + light.sky2 + " rules over the night.")
+console.log(light.sky3+ " are also in the sky.")
+console.log("The " + light.art + "; the artificial source of light, even long ago.")
+console.log("surprisingly, the glow " + light.glow + " can also produce some light.")
+
+let window={rich:"glas", poor:"metal",verypoor:"wood",witch:"cloth",thief:"no"}
+console.log("The rich: window of " +window.rich +".")
+console.log("The poor: window of " +window.poor +".")
+console.log("The very poor: window of " +window.verypoor +".")
+console.log("A witch: window of " +window.witch +".")
+console.log("A thief: "+window.thief +" window.")
+
+let alphabet={eng:"english", chai:"chinese", hin:"hindu", fre:"french", hev:"heaven",}
+console.log("The "+ alphabet.eng + " alphabet.")
+console.log( "The "+ alphabet.chai + " alphabet.")
+console.log("The "+ alphabet.hin + " alphabet.")
+console.log("The "+ alphabet.fre + " alphabet.")
+console.log("The "+ alphabet.hev + " alphabet.")
+
+let color={chel:"blue", blk:"black", man:"red", real:"white", es:"maroon",}
+
+console.log("Chelsea likes " +color.chel+ ".")
+console.log("Blackban likes " +color.blk+ ".")
+console.log("Manunited likes " +color.man+ ".")
+console.log("realmadrid likes " +color.real+ ".")
+console.log("Esroma likes " +color.es+ ".")
