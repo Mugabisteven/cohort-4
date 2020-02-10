c0679b48f93649fa134abd6f2fa52cacf0c12344
dwapenyi <60208963+dwapenyi@users.noreply.github.com>
2020-02-10 16:02:45 +0300

    Add files via upload
    OOP JS file.

---
+++ David.oop.js
@@ -0,0 +1,35 @@
+
+let car ={name: "Toyota", model: "Corolla", engine:"Petrol", displacement: "1498",doors: "4"}
+console.log (car.name)
+console.log (car.model)
+console.log (car.engine)
+console.log (car.displacement)
+console.log (car.doors)
+
+let plane ={nameP: "Boeing", modelP: "777", enginesP:"gen110B", thrustP: "110000",passengersP: "350"}
+console.log (plane.nameP)
+console.log (plane.modelP)
+console.log (plane.enginesP)
+console.log (plane.thrustP)
+console.log (plane.passengersP)
+
+let phone ={namePh: "Google", modelPh: "Pixel3", cpuPh:"Snapdragon 845", ramPh: "4GB",cameraPh: "12.2MP"}
+console.log (phone.namePh)
+console.log (phone.modelPh)
+console.log (phone.cpuPh)
+console.log (phone.ramPh)
+console.log (phone.cameraPh)
+
+let house ={nameH: "Tudor", floorsH: "2", windowsH:"20", toiletsH: "3",poolH: "yes"}
+console.log (house.nameH)
+console.log (house.floorsH)
+console.log (house.windowsH)
+console.log (house.toiletsH)
+console.log (house.poolH)
+
+let computer ={nameC: "HP", cpuC: "CoreI7", osC:"Windows 10", hdC: "1 TB",screenC: "22 inch"}
+console.log (computer.nameC)
+console.log (computer.cpuC)
+console.log (computer.osC)
+console.log (computer.hdC)
+console.log (computer.screenC)
