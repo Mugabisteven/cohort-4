0e775b2a42dd866ca36b9b8770d844883f240e68
Inezarachel <60212560+Inezarachel@users.noreply.github.com>
2020-02-10 06:09:19 -0800

    Add files via upload

---
+++ racheloop.js
@@ -0,0 +1,40 @@
+//Defining object faucet with five properties
+let faucet={name:"ballfaucet", use:"kitchen", color:"silver", speed:"fast", make:"spout"}
+//printing the results for 5 properties for object faucet
+console.log(faucet.name)
+console.log(faucet.use)
+console.log(faucet.color)
+console.log(faucet.speed)
+console.log(faucet.make)
+//Defining object fork with five properties
+let fork={type:"desert", numoflegs:3, size:"small", weight:"2gms", material:"silver"}
+//printing the results for 5 properties for object fork
+console.log(fork.type)
+console.log(fork.numoflegs)
+console.log(fork.size)
+console.log(fork.weight)
+console.log(fork.material)
+//Defining object leaf with five properties
+let leaf={name:"mango leaf", type:"compound", color:"dark green", size:"medium", blade:"smooth"}
+//printing the results for 5 properties for object leaf
+console.log(leaf.name)
+console.log(leaf.type)
+console.log(leaf.color)
+console.log(leaf.size)
+console.log(leaf.blade)
+//Defining object fruit with five properties
+let fruit={name:"tangarine", type:"citrus", size:"small sized", location:"morocco", nutrition:"vitaminc"}
+//printing the results for 5 properties for object fruit
+console.log(fruit.name)
+console.log(fruit.type)
+console.log(fruit.size)
+console.log(fruit.location)
+console.log(fruit.nutrition)
+//Defining object fruit with five properties
+let fruit={name:"pomegranate", type:"parfianka", size:"small", seeds:"613", family:"lythracease" }
+//printing the results for 5 properties for object fruit
+console.log(fruit.name)
+console.log(fruit.type)
+console.log(fruit.size)
+console.log(fruit.seeds)
+console.log(fruit.family)
