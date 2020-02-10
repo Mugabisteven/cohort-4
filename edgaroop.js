9ae7845f6fbd34a125f7d4994d568e3b4def29da
Edgar Mwambutsa <edgarhitts@gmail.com>
2020-02-10 19:53:09 +0300

    Add files via upload

---
+++ edgaroop.js
@@ -0,0 +1,40 @@
+//A categorized object named disease with different properties
+let disease={name: "corona", kind: "viral", country: "china", deathtoll: "900", infected: "30000"}
+
+console.log (disease.name)
+console.log (disease.kind)
+console.log (disease.country)
+console.log (disease.deathtoll)
+console.log (disease.infected)
+//A categorized object named country with different properties
+
+let country={name: "Uganda", population: "42m", language: "luganda", continent: "africa", president: "museveni"}
+console.log (country.uganda)
+console.log (country.population)
+console.log (country.language)
+console.log (country.continent)
+console.log (country. president)
+//A categorized object named actor with different properties
+
+let actor= {name: "denzel", country: "usa", race: "black", movie: "equalizer", age: "65"}
+console.log(actor.name)
+console.log(actor.country)
+console.log(actor.race)
+console.log(actor.movie)
+console.log(actor.age)
+//A categorized object named president with different properties
+
+let president={name: "trump", country: "usa", age: "73", party: "republican", wife: "melania"}
+console.log(president.name)
+console.log(president.country)
+console.log(president.age)
+console.log(president.party)
+console.log(president.wife)
+//A categorized object named comedian with different properties
+
+let comedian={name: "trevornoah", country: "southafrica", age: "35", show: "thedailyshow", book: "bornacrime"}
+console.log(comedian.name)
+console.log(comedian.country)
+console.log(comedian.age)
+console.log(comedian.show)
+console.log(comedian.book)
