e7e46682d827a7e799580f5548f59b84963ae137
Wamala-Emmanuel <kisaakye.emma@gmail.com>
2020-02-10 05:54:45 -0800

    Objects in JavaScript OOP

---
+++ EmmanuelWOOP.js
@@ -0,0 +1,40 @@
+//Define object table
+let table = {colour:"Red", legs:3, shape:"Rectangle", material:"Wooden", size:"Medium"}
+console.log(table.colour)
+console.log(table.legs)
+console.log(table.shape)
+console.log(table.material)
+console.log(table.size)
+
+//Define object chair
+let chair = {colour:"Blue", legs:4, shape:"Square", material:"Plastic", size:"Small"}
+//Display attribute value on console
+console.log(chair.colour)
+console.log(chair.legs)
+console.log(chair.shape)
+console.log(chair.material)
+console.log(chair.size)
+
+//Define object meal
+let meal = {colour:"White", amount:"0.2Kgs", shape:"oval", make:"Boiled", taste:"Sweet"}
+console.log(meal.colour)
+console.log(meal.amount)
+console.log(meal.shape)
+console.log(meal.make)
+console.log(meal.taste)
+
+//Define object money
+let money = {amount:1000, currency:"Shillings", shape:"Rectangle", material:"Paper", colour:"Brown"}
+console.log(money.colour)
+console.log(money.currency)
+console.log(money.shape)
+console.log(money.material)
+console.log(money.amount)
+
+//Define object song
+let song = {genre:"Jazz", artist:"Kenny G", duration:"3:00", ratings:"5/10", size:"3.4MBs"}
+console.log(song.genre)
+console.log(song.artist)
+console.log(song.duration)
+console.log(song.ratings)
+console.log(song.size)
