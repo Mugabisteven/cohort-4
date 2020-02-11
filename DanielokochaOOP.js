ec5292cccfbfc3a7f842ed2a7fde811b058a9910
Daniel-Tech4 <60209979+Daniel-Tech4@users.noreply.github.com>
2020-02-10 21:43:08 -0800

    Added strings on to the printed object bird and smartphone properties.

--- DanielokochaOOP.js
+++ DanielokochaOOP.js
@@ -1,25 +1,25 @@
 // Declaring an object smartphone with ist five properties and there values.
 let smartphone={companyname:"Itel",model:"A14",ROM:"8GB",RAM:"512MB",buildNumber:"v049"}
 // Print all the five properties and values of the declaring object smartphone on the screen. 
-console.log(smartphone.companyname)
-console.log(smartphone.model)
-console.log(smartphone.buildNumber)
-console.log(smartphone.RAM)
-console.log(smartphone.ROM)
+console.log(smartphone.companyname + " is a phones manufacturing company.")
+console.log(smartphone.model + " is an Itel smart phone model.")
+console.log(smartphone.buildNumber + " is the build number for Itel A14.")
+console.log("Itel A14 has a RAM of " + smartphone.RAM)
+console.log(smartphone.ROM + " is the total memory space for Itel A14.")
 // Declaring object bird with its five properties and there value.
-let bird={name:"chicken",wing:"two",leg:"two",peak:"one",eye:"two",}
+let bird={name:"chicken",wing:"two",leg:"two",mouth:"peak",eye:"two",}
 // Print all the declaring objects' properties  and values  on the screen.
-console.log(bird.name)
-console.log("Chicken's legs are" + bird.leg)
-console.log("Chicken eats with" + bird.peak)
-console.log(bird.eye)
-console.log(bird.wing)
+console.log(bird.name + " has nice meat taste")
+console.log("Chicken's legs are " + bird.leg)
+console.log("Chicken's " + bird.mouth + " is use for eating")
+console.log("Chicken has " + bird.eye + " eyes for seeing")
+console.log("This " + bird.wing + " wings is use for flying.")
 // Declaring object animal with five properties and there values. 
-let animal={Type:"wildAnimal",name:"lion",Legs:"four",body:"hairy",food:"only meat",}
+let animal={Type:"wildAnimal",name:"lion",Legs:"four",body:"hairy",food:" meat only",}
 // Printing the object prperties and there values on the screen.
-console.log("Animal kindom has two types one is" + animal.Type)
+console.log("Animal kindom has two types one is " + animal.Type)
 console.log(animal.name + " is a curnivorus animal.")
-console.log("Lion's legs is" + animal.Legs)
+console.log("Lion has " + animal.Legs + "legs.")
 console.log("The body is " + animal.body)
 console.log("It likes " + animal.food)
 // Declaring soil as an object with five properties and values.
@@ -35,7 +35,7 @@ let continent={Name:"Africa",Region:"EastAfrica",country:"Uganda",CapitalCity:"K
 // Print the declaring object properties and there values on the screen.
 console.log(continent.Name + " is a black continent.")
 console.log(continent.Region + " is one of the regions in Africa.")
-console.log(continent.country  + " ia an African country.")
+console.log(continent.country  + " is an African country.")
 console.log(continent.CapitalCity + " is the capital city of Uganda.")
 console.log(continent.president + " is the president of the republic of Uganda.")
 
