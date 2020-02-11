8c8a6a1f4c892ee2c1c4e1eb7280c49b4579274f
Inezarachel <60212560+Inezarachel@users.noreply.github.com>
2020-02-11 02:30:00 -0800

    Add files via upload

--- racheloop.js
+++ racheloop.js
@@ -1,40 +1,75 @@
-//Defining object faucet with five properties
-let faucet={name:"ballfaucet", use:"kitchen", color:"silver", speed:"fast", make:"spout"}
-//printing the results for 5 properties for object faucet
-console.log(faucet.name)
-console.log(faucet.use)
-console.log(faucet.color)
-console.log(faucet.speed)
-console.log(faucet.make)
-//Defining object fork with five properties
-let fork={type:"desert", numoflegs:3, size:"small", weight:"2gms", material:"silver"}
-//printing the results for 5 properties for object fork
-console.log(fork.type)
-console.log(fork.numoflegs)
-console.log(fork.size)
-console.log(fork.weight)
-console.log(fork.material)
-//Defining object leaf with five properties
-let leaf={name:"mango leaf", type:"compound", color:"dark green", size:"medium", blade:"smooth"}
-//printing the results for 5 properties for object leaf
-console.log(leaf.name)
-console.log(leaf.type)
-console.log(leaf.color)
-console.log(leaf.size)
-console.log(leaf.blade)
-//Defining object fruit with five properties
-let fruit={name:"tangarine", type:"citrus", size:"small sized", location:"morocco", nutrition:"vitaminc"}
-//printing the results for 5 properties for object fruit
-console.log(fruit.name)
-console.log(fruit.type)
-console.log(fruit.size)
-console.log(fruit.location)
-console.log(fruit.nutrition)
-//Defining object fruit with five properties
-let fruit={name:"pomegranate", type:"parfianka", size:"small", seeds:"613", family:"lythracease" }
-//printing the results for 5 properties for object fruit
-console.log(fruit.name)
-console.log(fruit.type)
-console.log(fruit.size)
-console.log(fruit.seeds)
-console.log(fruit.family)
+// //Defining object faucet with five properties
+// let faucet={name:"ballfaucet", use:"kitchen", color:"silver", speed:"fast", make:"spout"}
+// //Printing the results for 5 properties for object faucet
+// console.log(faucet.name)
+// console.log(faucet.use)
+// console.log(faucet.color)
+// console.log(faucet.speed)
+// console.log(faucet.make)
+// //Defining object fork with five properties
+// let fork={type:"desert", numoflegs:3, size:"small", weight:"2gms", material:"silver"}
+// //Printing the results for 5 properties for object fork
+// console.log(fork.type)
+// console.log(fork.numoflegs)
+// console.log(fork.size)
+// console.log(fork.weight)
+// console.log(fork.material)
+// //Defining object leaf with five properties
+// let leaf={name:"mango leaf", type:"compound", color:"dark green", size:"medium", blade:"smooth"}
+// //Printing the results for 5 properties for object leaf
+// console.log(leaf.name)
+// console.log(leaf.type)
+// console.log(leaf.color)
+// console.log(leaf.size)
+// console.log(leaf.blade)
+// //Defining object fruit with five properties
+// let fruit={name:"tangarine", type:"citrus", size:"small sized", location:"morocco", nutrition:"vitaminc"}
+// //Printing the results for 5 properties for object fruit
+// console.log(fruit.name)
+// console.log(fruit.type)
+// console.log(fruit.size)
+// console.log(fruit.location)
+// console.log(fruit.nutrition)
+// //Defining object fruit with five properties
+// let fruit={name:"pomegranate", type:"parfianka", size:"small", seeds:"613", family:"lythracease" }
+// //Printing the results for 5 properties for object fruit
+// console.log(fruit.name)
+// console.log(fruit.type)
+// console.log(fruit.size)
+// console.log(fruit.seeds)
+//console.log(fruit.family)
+//redoing the previous work to have instances of 
+
+//defining a method for object fruit with five properties and creating three instances of this object.
+function fruit()
+{
+    this.name
+    this.family
+    this.no_of_seeds
+    this.nutri_val
+    this.origin
+}
+
+var orange = new fruit()
+orange.name="orange"
+orange.family="citrus"
+orange.no_of_seeds=12
+orange.nutri_val="vitC"
+orange.origin="south america"
+
+var apple = new fruit()
+apple.name="apple"
+apple.family="fleshy"
+apple.no_of_seeds=2
+apple.nutri_val="carbs"
+apple.origin="central asia"
+
+var apricot = new fruit()
+apricot.name="apricot"
+apricot.family="fleshy"
+apricot.no_of_seeds=1
+apricot.nutri_val="vitA"
+apricot.origin="tropics"
+console.log(apple)
+console.log(orange)
+console.log(apricot)
