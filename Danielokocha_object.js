28e92b0c39745b8a2c56bd08d4956c74e6d11a51
Daniel-Tech4 <60209979+Daniel-Tech4@users.noreply.github.com>
2020-02-11 09:17:03 -0800

    Declaring object function with instance

---
+++ Danielokocha_object.js
@@ -0,0 +1,130 @@
+/** Defind an object call continent.
+var continent=function continent()
+{
+    this.name="Africa"
+    this.countries="53"
+    this.color // this is a key word in js to defind the properties of an object.
+    this.regions
+    this.mountaines
+    this.no_rivers
+    this.bigstCountry
+
+}
+// var europe is assigned to new continent
+var europe=new continent() // new mean we are creating the instens of new object "object"
+europe.name="europe"
+europe.no_oceans=2
+europe.no_rivers=6
+europe.bigstCountry='Russia'
+europe.population='millions of people'
+europe.peopleColor="White"
+
+var Assia=new continent()
+Assia.
+
+console.log(europe.name)
+console.log(europe.no_oceans)
+console.log(europe)**/
+
+// Defind an object call fruit with its instance.
+var fruit=function fruit()
+{
+    this.name
+    this.number_of_seeds
+    this.shape
+    this.size
+    this.color
+}
+var Mango=new fruit()
+Mango.name='Mango'
+Mango.number_of_seed='1 seed'
+Mango.shape='Oval'
+Mango.size='xl is 35in'
+Mango.color='Yellow_orange'
+console.log(Mango)
+var pawpaw=new fruit()
+    
+pawpaw.name='pawpaw'
+pawpaw.number_of_seeds='many seeds'
+pawpaw.shape='oval'
+pawpaw.size='2.5 inch'
+pawpaw.color='yellow fleshy'
+console.log(pawpaw)
+
+    var orange=new fruit()
+
+        orange.name='orange'
+        orange.number_of_seeds=''
+        orange.shape='round'
+        orange.size='2.5 inches'
+        orange.color='light orange'
+        console.log(orange)
+        
+// Defind an object call fruit with its instance.
+var secondarySchool=function secondarySchool()
+{
+    this.schoolName
+    this.location
+    this.headteacher
+    this.motor
+    this.subjects
+}
+var OurLadyOfAfricaNamilyango=new secondarySchool()
+OurLadyOfAfricaNamilyango.schoolName='Our_Lady_of_Africa_Namilyango'
+OurLadyOfAfricaNamilyango.location='seeta_Mukono'
+OurLadyOfAfricaNamilyango.headteacher='Lubega'
+OurLadyOfAfricaNamilyango.motor='strive_for_future'
+OurLadyOfAfricaNamilyango.subjects='physic,chemistry,biology,math'
+console.log(OurLadyOfAfricaNamilyango)
+
+var stJanani=new secondarySchool()
+stJanani.schoolName='St_Janani'
+stJanani.location='kabalagala'
+stJanani.headteacher=''
+stJanani.motor='We strive to excell'
+stJanani.subjects='physic,chmistry,history,math'
+console.log(stJanani)
+
+var GuluHighSchool=new secondarySchool()
+GuluHighSchool.schoolName='Gulu_High_School'
+GuluHighSchool.location='Gulu'
+GuluHighSchool.headteacher='saul'
+GuluHighSchool.motor='Trust_in God'
+GuluHighSchool.subjects='history,physic,agric,biology'
+console.log(GuluHighSchool)
+
+
+// Defind an object call fruit with its instance.
+var footballPlayer=function footballPlayer()
+{
+    this.full_name
+    this.genda
+    this.age
+    this.role
+    this.color
+}
+var Daniel=new footballPlayer()
+Daniel.full_name='Daniel_okocha'
+Daniel.genda='Male'
+Daniel.age='24'
+Daniel.role='central_defence'
+Daniel.color='black'
+
+console.log(Daniel)
+
+var Mark=new footballPlayer()
+Mark.full_name='Mark_deen'
+Mark.genda='Male'
+Mark.age='28'
+Markrole='goal_keeper'
+Mark.color='black'
+console.log(Mark)
+
+var Willy=new footballPlayer()
+Willy.full_name='Willy paul'
+Willy.genda='Male'
+Willy.age='34'
+Willy.role='striker'
+Willy.color='white'
+console.log(Willy)
+
