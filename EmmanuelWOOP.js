e2f513289b213937c6f6df8afefe4b186314d406
Wamala-Emmanuel <kisaakye.emma@gmail.com>
2020-02-11 13:00:18 +0300

    Update EmmanuelWOOP.js

--- EmmanuelWOOP.js
+++ EmmanuelWOOP.js
@@ -1,4 +1,4 @@
-//Define object table
+/** //Define object table
 let table = {colour:"Red", legs:3, shape:"Rectangle", material:"Wooden", size:"Medium"}
 console.log(table.colour)
 console.log(table.legs)
@@ -38,3 +38,39 @@ console.log(song.artist)
 console.log(song.duration)
 console.log(song.ratings)
 console.log(song.size)
+**/
+
+//Defining object continent
+function continent(){
+    this.name
+    this.size
+    this.population
+    this.numberOfCountries
+    this.compassDirection
+}
+//Declaring and instantiating a variable
+var europe = new continent()
+//Initialising variable attributes
+    europe.name = "Europe"
+    europe.size = 2
+    europe.population = 7
+    europe.numberOfCountries = 12
+    europe.compassDirection = "West"
+    console.log(europe)
+
+var asia = new continent()
+    asia.name = "Asia"
+    asia.size = 2
+    asia.population = 3
+    asia.numberOfCountries = 45
+    asia.compassDirection = "East" 
+//Displaying instance variable
+console.log(asia)
+
+var africa = new continent()
+    africa.name = "Africa"
+    africa.size = 5
+    africa.population = 33
+    africa.numberOfCountries = 41
+    africa.compassDirection = "North" 
+console.log(africa)
