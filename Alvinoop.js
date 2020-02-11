8d78306cd574c1682d7eef0f683293cb5add37b6
BatoAlvin <60175917+BatoAlvin@users.noreply.github.com>
2020-02-11 12:33:03 +0300

    Updated comments

--- Alvinoop.js
+++ Alvinoop.js
@@ -1,32 +1,42 @@
+//Declared object movies and intialised it with properties.
 let movies = {Genre:"horror", Duration:"1hr 30mins",Ratings:5.3/10,Year:2019,Size:698} 
+//Printed out properties for object movies.
       console.log(movies.Genre)
       console.log(movies.Duration)
       console.log(movies.Ratings)
       console.log(movies.Year)
       console.log(movies.Size)
 
+//Declared object dog and intailised it with its properties.
 let dog = {Name:"Rex", Breed:"Mongolian",Age:5,Category:"khavsar",Appearance:"Bushy tail"} 
+//Printed out object dog and its properties.
       console.log(dog.Name)
       console.log(dog.Breed)
       console.log(dog.Age)
       console.log(dog.Category)
       console.log(dog.Appearance)
 
+//Declared object phone and intialised it with its properties.
 let phone = {Storage:"8gb", Version:7.0,Language:"English",Devicename:"Tecnowx3P",Screenresolution:854*480} 
+//Printed out properties for object phone.
       console.log(phone.Storage)
       console.log(phone.Version)
       console.log(phone.Language)
       console.log(phone.Devicename)
       console.log(phone.Screenresolution)
 
+//Declared object headphones and intialised it with its properties.
 let headphones = {Brand:"JBL", color:"white",weight:"0.05kg",Connection:"wireless",Size:"medium"} 
+//Printed out properties for object headphones.
       console.log(headphones.Brand)
       console.log(headphones.color)
       console.log(headphones.weight)
       console.log(headphones.Connection)
       console.log(headphones.Size)
 
+//Declared object bulb and intialised it with its properties.
 let bulb = {color:"white", wattage:"40amps",Diameter:"16mm",weight:"0.4kg",make:"China"} 
+//Printed out properties for object bulb.
       console.log(bulb.color)
       console.log(bulb.wattage)
       console.log(bulb.Diameter)
