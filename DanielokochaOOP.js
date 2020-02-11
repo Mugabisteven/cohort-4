78219c5b064e33c77b9e7218ffd4b4d59e00cadd
Daniel-Tech4 <60209979+Daniel-Tech4@users.noreply.github.com>
2020-02-11 00:13:05 -0800

    Added strings on to the printed object bird and smartphone properties.

--- DanielokochaOOP.js
+++ DanielokochaOOP.js
@@ -6,6 +6,7 @@ console.log(smartphone.model + " is an Itel smart phone model.")
 console.log(smartphone.buildNumber + " is the build number for Itel A14.")
 console.log("Itel A14 has a RAM of " + smartphone.RAM)
 console.log(smartphone.ROM + " is the total memory space for Itel A14.")
+
 // Declaring object bird with its five properties and there value.
 let bird={name:"chicken",wing:"two",leg:"two",mouth:"peak",eye:"two",}
 // Print all the declaring objects' properties  and values  on the screen.
@@ -14,6 +15,7 @@ console.log("Chicken's legs are " + bird.leg)
 console.log("Chicken's " + bird.mouth + " is use for eating")
 console.log("Chicken has " + bird.eye + " eyes for seeing")
 console.log("This " + bird.wing + " wings is use for flying.")
+
 // Declaring object animal with five properties and there values. 
 let animal={Type:"wildAnimal",name:"lion",Legs:"four",body:"hairy",food:" meat only",}
 // Printing the object prperties and there values on the screen.
@@ -22,6 +24,7 @@ console.log(animal.name + " is a curnivorus animal.")
 console.log("Lion has " + animal.Legs + "legs.")
 console.log("The body is " + animal.body)
 console.log("It likes " + animal.food)
+
 // Declaring soil as an object with five properties and values.
 let soil={type:"loamsoil",color:"dark",layer:"top",uses:"for growing crops",weight:"40-40-20 ratio"}
 // Printing the object prperties and there values on the screen.
@@ -30,6 +33,7 @@ console.log("The color of loam soil is " + soil.color)
 console.log("Loam soil is on the " + soil.layer)
 console.log("Loam soil is mostly use " + soil.uses)
 console.log(soil.weight + " is the actual weight of loam soil. ")
+
 // Declaring the object continent with its five properties and values.
 let continent={Name:"Africa",Region:"EastAfrica",country:"Uganda",CapitalCity:"Kampala",president:"Museveni",}
 // Print the declaring object properties and there values on the screen.
