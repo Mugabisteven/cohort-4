8d4f96cdba5a3682f06022f337f0eb45160f7426
chrisopiyo <60352290+chrisopiyo@users.noreply.github.com>
2020-02-12 10:58:35 +0300

    Opiyo

---
+++ opiyo2-oop.js
@@ -0,0 +1,36 @@
+/**
+  function for the following city analysis of location
+  continent,land size and population
+ **/
+function city()
+{
+    this.name
+    this.location
+    this.continent
+    this.landArea
+    this.population
+}
+
+var newYork = new city()
+newYork.name = "NewYork"
+newYork.location = "USA"
+newYork.continent = "North America"
+newYork.landArea = "783.8sq km"
+newYork.population = "8.5million people"
+console.log( newYork )
+
+var johannesburg = new city()
+johannesburg.name = "Johannesburg"
+johannesburg.location = "South Africa"
+johannesburg.continent = "Africa"
+johannesburg.landArea = "1,645sq km"
+johannesburg.population = "4.4million people"
+console.log(johannesburg)
+
+var seoul = new city()
+seoul.name = "Seoul"
+seoul.location = "South Korea"
+seoul.continent = "Asia"
+seoul.landArea = "605.2sq km"
+seoul.population = "10.2million people"
+console.log(seoul)
