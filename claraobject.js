68251f6cc0b7cd25e109a0520d999377d82b4de0
claraasiimwe <60213046+claraasiimwe@users.noreply.github.com>
2020-02-12 17:28:42 +0300

    object oriented assignment

--- claraobject.js
+++ claraobject.js
@@ -1,94 +1,110 @@
 //* 
-function continent()
-{
-    this.name
-    this.biggestcountry
-    this.population
-    this.largestwaterbody
-    this.export
+// function continent()
+// {
+//     this.name
+//     this.biggestcountry
+//     this.population
+//     this.largestwaterbody
+//     this.export
+// }
+// console.log(continent.name)
+
+// var europe= new continent()
+// europe.name='europe'
+// europe.no_oceans='2'
+// europe.no_rivers='6'
+// europe.biggestcountry='Russia'
+// console.log(europe.name) 
+
+
+// function tribesinuganda()
+// {
+// this.name
+// this.staplefood
+// this.language
+// this.traditionalwear
+// this.royals
+// }
+// console.log(tribesinuganda.name)
+
+// var baganda= new tribesinuganda()
+// baganda.name= 'baganda'
+// baganda.staplefood= 'matooke'
+// baganda.language= 'luganda'
+// baganda.traditionalwear= 'gomesi/kanzu'
+// baganda.royals= '50'
+// console.log(baganda.name)
+
+// var banyankole= new tribesinuganda()
+// banyankole.name= 'banyankole'
+// banyankole.staplefood= 'millet'
+// banyankole.language= 'runyankole'
+// banyankole.traditionalwear= 'mushanana'
+// banyankole.royals= '15'
+// console.log(banyankole.name)
+
+// var basoga= new tribesinuganda()
+// basoga.name= 'basoga'
+// basoga.staplefood= 'sweetpotatoes'
+// basoga.language= 'lusoga'
+// basoga.traditionalwear= 'gomesi'
+// basoga. royals= '30'
+// console.log(basoga.name)
+
+// var itesots= new tribesinuganda()
+// itesots.name= 'itesots'
+// itesots.staplefood= 'millet'
+// itesots.language= 'teso'
+// itesots.traditionalwear= 'gomesi'
+// itesots.royals= '40'
+// console.log(itesots.name)
+
+// var acholi= new tribesinuganda()
+// acholi.name= 'acholi'
+// acholi.staplefood= 'groundnuts'
+// acholi.traditionalwear= 'backcloth'
+// acholi.royals= '20'
+// console.log(acholi.name)
+
+// //
+// let animal= {pet:"dog", aquatic:"fish",wild:"lion"}
+// console.log("animal.pet")
+// console.log("animal.aquatic")
+// console.log("animal.wild")
+
+// let fruit= {sweet:"pineapples", watery:"watermelon", seedless:"banana"}
+// console.log("fruit.sweet")
+// console.log("fruit:watery")
+// console.log("fruit.seedless")
+
+// let roaduser= {car:"driver",bicycle:"cyclist",motorcycle:"motorcyclist" }
+// console.log("roaduser.car")
+// console.log("roaduser.bicycle")
+// console,log("roaduser.motorcycle")
+
+// let bible= {oldtestament:"genesis", newtestament:john}
+// console.log("bible.oldtestament")
+// console.log("bible.newtestament")
+
+// let email= {spam:"12",primary:"101", sent:"92", draft:"11"}
+// console.log("email.spam")
+// console.log("email.primary")
+// console.log("email.sent")
+// console.log("email.draft")
+
+//* 
+function country(name, location, population, language){
+this.name=name
+this.location=location
+this.population=population
+this.language=language
 }
-console.log(continent.name)
 
-var europe= new continent()
-europe.name='europe'
-europe.no_oceans='2'
-europe.no_rivers='6'
-europe.biggestcountry='Russia'
-console.log(europe.name) *//
+var uganda= new country('uganda', 'eastafrica', '42.86million','english')
+console.log(uganda.location)
 
+var nigeria= new country('nigeria','westafrica','190.9million','english')
+console.log(nigeria)
 
-function tribesinuganda()
-{
-this.name
-this.staplefood
-this.language
-this.traditionalwear
-this.royals
-}
-console.log(tribesinuganda.name)
-
-var baganda= new tribesinuganda()
-baganda.name= 'baganda'
-baganda.staplefood= 'matooke'
-baganda.language= 'luganda'
-baganda.traditionalwear= 'gomesi/kanzu'
-baganda.royals= '50'
-console.log(baganda.name)
-
-var banyankole= new tribesinuganda()
-banyankole.name= 'banyankole'
-banyankole.staplefood= 'millet'
-banyankole.language= 'runyankole'
-banyankole.traditionalwear= 'mushanana'
-banyankole.royals= '15'
-console.log(banyankole.name)
-
-var basoga= new tribesinuganda()
-basoga.name= 'basoga'
-basoga.staplefood= 'sweetpotatoes'
-basoga.language= 'lusoga'
-basoga.traditionalwear= 'gomesi'
-basoga. royals= '30'
-console.log(basoga.name)
-
-var itesots= new tribesinuganda()
-itesots.name= 'itesots'
-itesots.staplefood= 'millet'
-itesots.language= 'teso'
-itesots.traditionalwear= 'gomesi'
-itesots.royals= '40'
-console.log(itesots.name)
-
-var acholi= new tribesinuganda()
-acholi.name= 'acholi'
-acholi.staplefood= 'groundnuts'
-acholi.traditionalwear= 'backcloth'
-acholi.royals= '20'
-console.log(acholi.name)
-
-//
-let animal= {pet:"dog", aquatic:"fish",wild:"lion"}
-console.log("animal.pet")
-console.log("animal.aquatic")
-console.log("animal.wild")
-
-let fruit= {sweet:"pineapples", watery:"watermelon", seedless:"banana"}
-console.log("fruit.sweet")
-console.log("fruit:watery")
-console.log("fruit.seedless")
-
-let roaduser= {car:"driver",bicycle:"cyclist",motorcycle:"motorcyclist" }
-console.log("roaduser.car")
-console.log("roaduser.bicycle")
-console,log("roaduser.motorcycle")
-
-let bible= {oldtestament:"genesis", newtestament:john}
-console.log("bible.oldtestament")
-console.log("bible.newtestament")
-
-let email= {spam:"12",primary:"101", sent:"92", draft:"11"}
-console.log("email.spam")
-console.log("email.primary")
-console.log("email.sent")
-console.log("email.draft")
-
+var southafrica= new country('southafrica','southafrica','56.72million','afrikaans')
+console.log(southafrica)
